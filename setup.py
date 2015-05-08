718164b48aad1efa2e3cc1644375b3132c523dae
Christo Crampton <info@38.co.za>
2015-05-08 11:17:52 +0200

    bump the version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='tangent-tokenauth',
-    version='0.0.2',
+    version='0.0.3',
     author=u'Tangent Solutions',
     author_email=u'admin@tangentsolutions.co.za',
     packages=['tokenauth'],
